81abc2f3801a8714042ee4d7c212bbc98c0bf133
shiyulu <dev@shiyulu.com>
2026-08-07 13:46:07 +0800

    fix: bust stale frontend cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shiyulu-v23';
+const CACHE = 'shiyulu-v24';
 const BASE = new URL('./', self.location.href);
 const OFFLINE_URL = new URL('index.html', BASE).toString();
 const ASSETS = [
